1a19161701df29f6ede48c2398edf81e89abd64a
Diana Chirtop <chirtop.diana@yahoo.com>
2023-03-31 15:34:50 +0300

    Initial commit

---
+++ firstMacApp/firstMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstMacAppApp.swift
+//  firstMacApp
+//
+//  Created by Diana on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct firstMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
